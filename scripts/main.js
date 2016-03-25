af4fd804fd140144a314eb15db76a24b3d9f656b
Tom Liversidge <tliversidge@roishoppermarketing.com>
2016-03-25 07:58:48 +0000

    Adding styling to header

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,17 @@
 var React = require('react');
-var ReactDOM = require('react-dom')
+var ReactDOM = require('react-dom');
+
+/*
+ App
+ */
 
 var App = React.createClass({
-    render: function () {
+
+    render : function() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header/>
+                    <Header tagline="Fresh Seafood Market" />
                 </div>
                 <Order/>
                 <Inventory/>
@@ -15,41 +20,68 @@ var App = React.createClass({
     }
 });
 
+/*
+ Header
+ <Header/>
+ */
 var Header = React.createClass({
-    render: function(){
+    render : function() {
+        return (
+            <header className="top">
+                <h1>Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+                    Day</h1>
+                <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+            </header>
+        )
+    }
+})
+
+/*
+ Order
+ <Order/>
+ */
+var Order = React.createClass({
+    render : function() {
         return (
             <p>Order</p>
         )
     }
-});
+})
 
-var Order = React.createClass({
-    render: function(){
-        return (
-            <p>Header</p>
-        )
-    }
-});
-
+/*
+ Inventory
+ <Inventory/>
+ */
 var Inventory = React.createClass({
-    render: function(){
+    render : function() {
         return (
             <p>Inventory</p>
         )
     }
-});
+})
+
+
+/*
+ StorePicker
+ This will let us make <StorePicker/>
+ */
+
 var StorePicker = React.createClass({
 
-    render: function () {
-
+    render : function() {
         return (
             <form className="store-selector">
-                <h2>Please enter a store</h2>
-                <input type="text" ref="storeId" required/>
-                <input type="submit"/>
+                <h2>Please Enter A Store</h2>
+                <input type="text" ref="storeId" required />
+                <input type="Submit" />
             </form>
         )
     }
+
 });
 
-ReactDOM.render(<App/>, document.getElementById('main'))
+ReactDOM.render(<App/>, document.querySelector('#main'));
