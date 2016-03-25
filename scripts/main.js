cd2776140d4ddfa9c88e0fdeeb14a089acdb1f0f
Tom Liversidge <tliversidge@roishoppermarketing.com>
2016-03-25 07:39:45 +0000

    Adding three column basic layout

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,55 @@
 var React = require('react');
 var ReactDOM = require('react-dom')
 
+var App = React.createClass({
+    render: function () {
+        return (
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header/>
+                </div>
+                <Order/>
+                <Inventory/>
+            </div>
+        )
+    }
+});
+
+var Header = React.createClass({
+    render: function(){
+        return (
+            <p>Order</p>
+        )
+    }
+});
+
+var Order = React.createClass({
+    render: function(){
+        return (
+            <p>Header</p>
+        )
+    }
+});
+
+var Inventory = React.createClass({
+    render: function(){
+        return (
+            <p>Inventory</p>
+        )
+    }
+});
 var StorePicker = React.createClass({
 
-    render: function(){
+    render: function () {
 
-        return (        	
+        return (
             <form className="store-selector">
-            	<h2>Please enter a store</h2>
-	            <input type="text" ref="storeId" required/>
-	            <input type="submit"/>
+                <h2>Please enter a store</h2>
+                <input type="text" ref="storeId" required/>
+                <input type="submit"/>
             </form>
         )
     }
 });
 
-ReactDOM.render(<StorePicker/>, document.getElementById('main'))
+ReactDOM.render(<App/>, document.getElementById('main'))
