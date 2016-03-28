63f65c4c5170c7ae0d24539dabf8b30505ba33d6
Tom Liversidge <tliversidge@roishoppermarketing.com>
2016-03-28 16:10:20 +0100

    Display an itemized order summary

--- scripts/main.js
+++ scripts/main.js
@@ -79,7 +79,9 @@ var App = React.createClass({
                         {Object.keys(this.state.fishes).map(this.renderFish)}
                     </ul>
                 </div>
-                <Order/>
+                <Order
+                    fishes={this.state.fishes}
+                    order={this.state.order}/>
                 <Inventory loadSamples={this.loadSamples} addFish={this.addFish}/>
             </div>
         )
@@ -148,9 +150,45 @@ var Header = React.createClass({
  <Order/>
  */
 var Order = React.createClass({
-    render: function () {
+    renderOrder: function (key) {
+        var fish = this.props.fishes[key];
+        var count = this.props.order[key];
+
+        if (!fish) {
+            return <li key={key}>Sorry no fish available</li>
+        }
+
         return (
-            <p>Order</p>
+            <li><span>{count}</span>lbs
+                {fish.name}
+                <span className="price">{h.formatPrice(count * fish.price)}</span>
+            </li>
+        )
+    },
+    render: function () {
+        var orderIds = Object.keys(this.props.order);
+        var total = orderIds.reduce((prevTotal, key) => {
+            var fish = this.props.fishes[key];
+            var count = this.props.order[key];
+            var isAvailable = fish && fish.status === 'available';
+            if (fish && isAvailable) {
+                return prevTotal + (count * parseInt(fish.price) || 0)
+            }
+            ;
+            return prevTotal;
+
+        }, 0);
+        return (
+            <div className="order-wrap">
+                <h2 className="order-title">Your Order</h2>
+                <ul className="order">
+                    {orderIds.map(this.renderOrder)}
+                    <li className="total">
+                        <strong>Total:</strong>
+                        {h.formatPrice(total)}
+                    </li>
+                </ul>
+            </div>
         )
     }
 });
