e338439ce1f4fc727d8ca55ee4eb0ce504f89799
Tom Liversidge <tliversidge@roishoppermarketing.com>
2016-03-28 09:03:54 +0100

    Add button to load sample fishes from sample dataset

--- scripts/main.js
+++ scripts/main.js
@@ -28,6 +28,11 @@ var App = React.createClass({
             fishes: this.state.fishes
         })
     },
+    loadSamples: function(){
+        this.setState({
+            fishes: require('./sample-fishes')
+        });
+    },
     render: function () {
         return (
             <div className="catch-of-the-day">
@@ -35,7 +40,7 @@ var App = React.createClass({
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
                 <Order/>
-                <Inventory addFish={this.addFish}/>
+                <Inventory loadSamples={this.loadSamples} addFish={this.addFish}/>
             </div>
         )
     }
@@ -119,6 +124,7 @@ var Inventory = React.createClass({
             <div>
                 <h2>Inventory</h2>
                 <AddFishForm {...this.props}/>
+                <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
             </div>
         )
     }
